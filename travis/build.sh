cb0969f4c01fb24ff7c5297bf51be0fc1c330ec1
stlouisn <stlouisn@hotmail.com>
2019-04-16 17:43:35 -0300

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -9,6 +9,9 @@ architectures="amd64"
 for arch in $architectures
 do
 
+	# Login into docker
+	echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
+
 	# Build temporary image
 	buildctl build \
 		--frontend dockerfile.v0 \
@@ -16,27 +19,6 @@ do
 		--opt filename=./Dockerfile.${DOCKER_NAME}-${DOCKER_TAG}-$arch \
 		--local dockerfile=. \
 		--local context=. \
-		--output type=docker,name=tmp-image-$arch,dest=tmp-image-$arch.tar
-
-	# Load temporary image
-	docker load -i tmp-image-$arch.tar
-
-	# Run temporary image
-	docker create --name tmp-image-$arch tmp-image-$arch '/bin/bash -c exit'
-
-	# Extract flattened image
-	docker export -o import-image-$arch.tar tmp-image-$arch
-
-	# Create docker image
-	docker import \
-		import-image-$arch.tar \
-		--message 'Imported from ${DOCKER_NAME}/${DOCKER_TAG}' \
-		${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-$arch
-
-	# Login into docker
-	echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
-
-	# Push image to docker hub
-	docker push ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-$arch
+		--output type=image,name=docker.io/${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-$arch
 
 done
