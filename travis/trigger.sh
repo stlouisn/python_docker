1bd9337520b32437aafd82c63b8e4dbad357e743
stlouisn <stlouisn@hotmail.com>
2019-04-16 17:48:53 -0300

    Create trigger.sh

---
+++ travis/trigger.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Builds to trigger
+builds="beets_docker home-assistant_docker"
+
+body='{
+"request": {
+"message": "Push from stlouisn/ubuntu",
+"branch": "master"
+}}'
+
+for build in $builds
+do
+
+  # Trigger build
+  curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" \
+    -H "Authorization: token ${TRAVIS_API_TOKEN}" \
+    -d "$body" \
+    https://api.travis-ci.org/repo/${DOCKER_MAINTAINER}%2F$build/requests
+
+done
